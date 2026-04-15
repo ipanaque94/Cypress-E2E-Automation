7f131a91c887822725daf7a814c283c0b3ec2d1e
Enoc <rodasenoc4@gmail.com>
2026-04-15 16:05:33 -0500

    Fix Allure import y limpiar config Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -8,6 +8,7 @@ module.exports = defineConfig({
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: "cypress/support/e2e.js",
     setupNodeEvents(on, config) {
+      // Activa Allure
       allureCypress(on, config, {
         resultsDir: "allure-results",
       });
@@ -48,6 +49,8 @@ module.exports = defineConfig({
           });
         },
       });
+
+      return config;
     },
   },
   projectId: "6m8pac",
